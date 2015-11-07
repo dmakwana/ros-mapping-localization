fbf010727cfe986172def37f7bd5594a93739e96
Digvijay Makwana <digvijaymakwana@gmail.com>
2015-11-07 12:58:32 -0500

    mapping node works now, extra comments removed

--- src/mapping_node.cpp
+++ src/mapping_node.cpp
@@ -58,16 +58,14 @@ double init_ips_yaw;
 double ips_x;
 double ips_y;
 double ips_yaw;
-
+geometry_msgs::PoseStamped curr_pose_stamped;
 short sgn(int x) { return x >= 0 ? 1 : -1; }
 
 //Callback function for the Position topic (SIMULATION)
 void pose_callback(const gazebo_msgs::ModelStates& msg) 
 {
-    
     int i;
     for(i = 0; i < msg.name.size(); i++) if(msg.name[i] == "mobile_base") break;
-
     ips_x = msg.pose[i].position.x ;
     ips_y = msg.pose[i].position.y ;
     ips_yaw = tf::getYaw(msg.pose[i].orientation);
@@ -78,7 +76,15 @@ void pose_callback(const gazebo_msgs::ModelStates& msg)
         init_ips_yaw = ips_yaw;
         initial_pose_found = true;
     }
-
+    curr_pose_stamped.pose.position.x = msg.pose[i].position.x;
+    curr_pose_stamped.pose.position.y = msg.pose[i].position.y;
+    curr_pose_stamped.pose.position.z = msg.pose[i].position.z;
+    curr_pose_stamped.pose.orientation.x = msg.pose[i].orientation.x;
+    curr_pose_stamped.pose.orientation.y = msg.pose[i].orientation.y;
+    curr_pose_stamped.pose.orientation.z = msg.pose[i].orientation.z;
+    curr_pose_stamped.pose.orientation.w = msg.pose[i].orientation.w;
+    curr_pose_stamped.header.stamp = ros::Time::now();
+    curr_pose_stamped.header.seq++;
 }
 
 //Callback function for the Position topic (LIVE)
@@ -102,74 +108,40 @@ float logit(int prob) {
 
 //Callback function for the scan
 void scan_callback(const sensor_msgs::LaserScan& msg) {
-    // ROS_INFO("scan_callback!");
     int i = 0, x0 = 0, y0 = 0, x1 = 0, y1 = 0, j = 0, r = 0;
-    float angle;
-    float actual_angle;
-    // float *values = &msg.ranges[0];
-    // ROS_INFO("AT 0 is: %f", values[0]);
-    // ROS_INFO("Number of Data Points %d!", (int)msg.ranges.size());
+    float angle, actual_angle;
     for (i = 0; i < (int)msg.ranges.size(); i++) {
-        // ROS_INFO("%d: %f", i, msg.ranges.at(i));
-        // ROS_INFO("sup");
+        // Update cells only for where we have data
+        if (isnan(msg.ranges.at(i)) || 
+            msg.ranges.at(i) > msg.range_max || 
+            msg.ranges.at(i) < msg.range_min) { 
+            continue; 
+        }
         angle = msg.angle_min + i * msg.angle_increment;
-        actual_angle = ips_yaw - angle;
-        // Update cells for where we have data
-        // ROS_INFO("sup0 %f, %f", msg.range_min, msg.range_max);
-        if (isnan(msg.ranges.at(i)) || msg.ranges.at(i) > msg.range_max || msg.ranges.at(i) < msg.range_min) { continue; }
-        x0 = CELLS_PER_METER*(ips_x - init_ips_x) + (COLS-1)/2;
-        y0 = CELLS_PER_METER*(ips_y - init_ips_y) + (ROWS-1)/2;
+        actual_angle = ips_yaw + angle;
+        x0 = CELLS_PER_METER * (ips_x - init_ips_x) + (COLS-1)/2;
+        y0 = CELLS_PER_METER * (ips_y - init_ips_y) + (ROWS-1)/2;
         x1 = x0 + CELLS_PER_METER * msg.ranges[i] * cos(actual_angle);
         y1 = y0 + CELLS_PER_METER * msg.ranges[i] * sin(actual_angle);
         std::vector<int> x, y;
-        // ROS_INFO("INITIAL IPS_YAW: %f", init_ips_yaw*180/PI);
-        // ROS_INFO("IPS_YAW: %f", ips_yaw*180/PI);
-        // ROS_INFO("PHI: %f", angle*180/PI);
-        // ROS_INFO("ACTUAL ANGLE: %f", actual_angle*180/PI);
-        // ROS_INFO("RANGE: %f", msg.ranges.at(i));
-        // ROS_INFO("sup1 %f, %f, %f", cos(actual_angle),sin(actual_angle), (float)msg.ranges.at(i));
-        // ROS_INFO("sup2 %d, %d, %d, %d", x0 ,y0, x1, y1);
-        // ROS_INFO("sup3 yaw: %f, range: %f, phi %f,", ips_yaw ,msg.ranges.at(i), angle);
         bresenham(x0, y0, x1, y1, x, y);
-        // ROS_INFO("sup2");
-        // exit(-1);    
-        // ROS_INFO("vector_size: %d", (int)x.size());
         for (int j = 0; j < x.size(); j++) {
-            // ROS_INFO("HERE2");
             int x_delta = x[j]-x0;
             int y_delta = y[j]-y0;
             r = sqrt(x_delta*x_delta + y_delta*y_delta);
-            // ROS_INFO("R: %d", r);
-            // ROS_INFO("msg.range: %f", msg.ranges.at(i));
-            // ROS_INFO("%d, %d", x[j], y[j]);
             int new_prob;
-            if (r < (msg.ranges.at(i) * 10 - 2)) { 
+            if (r < (msg.ranges.at(i) * CELLS_PER_METER - 2)) { 
                 new_prob = LOW_PROB;
-                // ROS_INFO("LOW_PROB");
             } 
-            else { 
-                // ROS_INFO("HIGH_PROB");
+            else {
                 new_prob = HIGH_PROB; 
             }
-            // ROS_INFO("HERE");
             int old_prob = myMap[y[j]*COLS + x[j]];
-            // if (old_prob != 0) {
-            //     ROS_INFO("NOT ZERO");
-            // } else {
-            //     ROS_INFO("ZERO");
-            //     exit(-1);
-            // }
-            // ROS_INFO("OLD PROB AT %d, %d: %d", x[j], y[j], old_prob);
-            // ROS_INFO("OLD LOGIT: %f, NEW_LOGIT: %f", logit(old_prob), logit(new_prob));
-            // ROS_INFO("UPDATING_PROB: %d", new_prob); 
             
             float new_logit = logit(new_prob) + logit(old_prob);
             myMap[y[j]*COLS + x[j]] = get_prob_from_logit(new_logit);
-            int m = 1 + myMap[y[j]*COLS + x[j]];
-            // ROS_INFO("NEW PROB: %d", m);
         }
     }
-    // ROS_INFO("scan_callback finished!");
     return;
 }
 //Bresenham line algorithm (pass empty vectors)
@@ -243,11 +215,13 @@ int main(int argc, char **argv)
         ros::spinOnce();
     }
     ROS_INFO("Received initial pose!");
-
+    
     metadata.origin = initial_pose;
     metadata.map_load_time = ros::Time::now();
     grid.info = metadata;
     grid.header.frame_id = "base_footprint";
+    curr_pose_stamped.header.frame_id = "base_footprint";
+    curr_pose_stamped.header.seq = 0;
 
     ROS_INFO("Initializing grid cells!");
     for (int i = 0; i < ROWS*COLS; i++) {
@@ -275,10 +249,10 @@ int main(int argc, char **argv)
         grid.data = myMap;
         grid.header.stamp = ros::Time::now();
 
-    	velocity_publisher.publish(vel); // Publish the command velocity     
+    	// velocity_publisher.publish(vel); // Publish the command velocity     
         map_publisher.publish(grid);
+        pose_publisher.publish(curr_pose_stamped);
         grid.header.seq++;
     }
-
     return 0;
 }
